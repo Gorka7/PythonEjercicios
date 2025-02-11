658706fcfbe9433d342adc1f483a6e87f262ce22
gorka7 <gorksanchez@gmail.com>
2025-02-12 00:19:40 +0100

    Add files via upload

--- 11_Funciones.py
+++ 11_Funciones.py
@@ -4,7 +4,6 @@
 
 #Definición
 
-
 def my_function():
     print("Esto es una función")
 
@@ -12,20 +11,23 @@ my_function()
 
 #Función con parámetros de entrada/argumentos
 
-
 def sum_two_values (first_value , second_value):
     print(first_value + second_value)
 
 sum_two_values(5, 7)
 sum_two_values(54754, 71231)
 
-"""
+#Lo interpreta como cadena de caracteres y los concatena
+sum_two_values("5","7")
+
+#Le paso un entero , pero lo convierto en entero "int"
+sum_two_values(int("5"),int("7"))
+
 sum_two_values(1.4, 5.2)
-"""
+
 
 
 #Función con parámetros de entrada/argumentos y retorno
-
 
 def sum_two_values_with_return(first_value, second_value):
     my_sum = first_value + second_value
