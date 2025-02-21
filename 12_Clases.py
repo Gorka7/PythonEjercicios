1a35916fa666e948f35bd72ddfd17a5f467c39f5
gorka7 <gorksanchez@gmail.com>
2025-02-21 22:59:38 +0100

    Add files via upload

--- 12_Clases.py
+++ 12_Clases.py
@@ -1,28 +1,25 @@
 #Clase en vídeo: https://youtu.be/Kp4Mvapo5kc?t=29327
 
-###Classes###
+###Classes ###
 
-#Definición
+# Definición
 
 class MyEmptyPerson:
-    pass  
-    #Para poder dejar la clase vacía
-    print("Hola")
+    #pass  # Para poder dejar la clase vacía
+    def __init__(self):
+        print("Hola")        
+    
 
-
-print(MyEmptyPerson) # o print(MyEmptyPerson()) 
-
-
+print(MyEmptyPerson)
+print(MyEmptyPerson())
 
 #Clase con constructor, funciones y propiedades privadas y públicas
 
 
 class Person:
-
     def __init__(self, name, surname, alias="Sin alias"):
-        self.full_name = f"{name} {surname} ({alias})"  #Propiedad pública
-        self.__name = name  #Propiedad privada
-
+        self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública
+        self.__name = name  # Propiedad privada
 
     def get_name(self):
         return self.__name
@@ -44,18 +41,3 @@ print(my_other_person.full_name)
 
 my_other_person.full_name = 14
 print(my_other_person.full_name)
-
-#ClaseFormula1
-class Formula1:
-   def __init__(self,neumaticos,aerodinamica):
-    self.neumaticos = neumaticos
-    self.aerodinamica = aerodinamica
-
-
-mi_coche = Formula1("Blandos","Baja Carga")
-print(mi_coche.neumaticos)
-print(mi_coche.aerodinamica)
-#aston_martin = print("El coche de Fernando Alonso lleva nematicos "f"{mi_coche.neumaticos}")
-#print(aston_martin)
-
-   
