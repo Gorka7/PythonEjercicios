895dcedab420853e99a56515f4946e6aaa6c9622
gorka7 <gorksanchez@gmail.com>
2025-02-26 01:26:56 +0100

    "Add files via upload"

--- 14_Modulos.py
+++ 14_Modulos.py
@@ -1,21 +1,51 @@
 #Clase en vídeo: https://youtu.be/Kp4Mvapo5kc?t=34583
 
-###Modules
-from math import pi as PI_VALUE
-import math
-from my_module import sumValue, printValue
+#Modulos
+
+#from math import pi as PI_VALUE
+#import math
+
+
+#print (math.pi)
+#print (math.pow(2, 8))
+#print(PI_VALUE)
+
+print("--------------------------------------------------------------------")
+print("-                            Módulos                               -")
+print("--------------------------------------------------------------------")
+print("-   Con esto podemos coger porciones de codigo de otros archivos   -")
+print("-   Y reutilizarlo en otros programas.                             -")
+print("-   P.EMPL : con my_module                                         -")
+print("                                                                    ")
+print("-   Primero lo importamos :                                        -")
+print("--------------------------------------------------------------------")
+print("-   import my_module                                               -")
+print("--------------------------------------------------------------------")
+
 import my_module
+
+print("--------------------------------------------------------------------------------")
+print("-   Luego podemos llamar a las diferentes partes del codigo que hay dentro :   -")
+print("--------------------------------------------------------------------------------")
+print("-   my_module.sumValue(5, 3, 1)            -")
+print("-   my_module.printValue('Hola Python!')   -")
+print("--------------------------------------------")
 
 my_module.sumValue(5, 3, 1)
 my_module.printValue("Hola Python!")
 
+print("---------------------------------------------------------------------------")
+print("-   Otra forma de hacerlo es importar solo unicamente parte del modulo    -")
+print("---------------------------------------------------------------------------")
+print("-   importamos de la siguiente forma :                                    -")
+print("---------------------------------------------------------------------------")
+print("-   'from my_module import sumValue'                                      -")
+print("---------------------------------------------------------------------------")
+print("-   sumValue(5, 3, 1)                                                     -")
+print("---------------------------------------------------------------------------")   
+
+from my_module import sumValue
+#from my_module import sumValue, printValue
 
 sumValue(5, 3, 1)
-printValue("Hola python")
-
-
-print (math.pi)
-print (math.pow(2, 8))
-
-
-print(PI_VALUE)
+#printValue("Hola python")
