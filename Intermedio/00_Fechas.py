b3961d07bc7bfb08f43f6e00c37349ce573671cc
Gorka7 <gorksanchez@gmail.com>
2025-03-02 20:06:59 +0100

    "Add files via upload"

--- Intermedio/00_Fechas.py
+++ Intermedio/00_Fechas.py
@@ -23,54 +23,8 @@ timestamp = ahora.timestamp()
 print(timestamp)
 
 
-"""
+
 year_2023 = datetime(2023, 1, 1)
 
-print_date(year_2023)
+#Prueba
 
-#Time
-
-
-current_time = time(21, 6, 0)
-
-print(current_time.hour)
-print(current_time.minute)
-print(current_time.second)
-
-#Date
-
-
-current_date = date.today()
-
-print(current_date.year)
-print(current_date.month)
-print(current_date.day)
-
-current_date = date(2022, 10, 6)
-
-print(current_date.year)
-print(current_date.month)
-print(current_date.day)
-
-current_date = date(current_date.year,03    
-                    current_date.month + 1, current_date.day)
-
-print(current_date.month)
-
-#Operaciones con fechas
-
-diff = year_2023 - now
-print(diff)git
-
-diff = year_2023.date() - current_date
-print(diff)
-
-#Timedelta
-
-
-start_timedelta = timedelta(200, 100, 100, weeks=10)
-end_timedelta = timedelta(300, 100, 100, weeks=13)
-
-print(end_timedelta - start_timedelta)
-print(end_timedelta + start_timedelta)
-"""
